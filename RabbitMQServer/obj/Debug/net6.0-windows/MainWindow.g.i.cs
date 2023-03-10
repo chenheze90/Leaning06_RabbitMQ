c724561e0beece3e6b1933d63e901360391d301c
chenheze90 <602797395@qq.com>
2023-03-10 11:30:45 +0800

    初始上传

--- RabbitMQServer/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ RabbitMQServer/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace RabbitMQServer {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RabbitMQServer;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RabbitMQServer;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- RabbitMQTest/obj/Debug/net6.0-windows/PowerLine.g.i.cs
+++ RabbitMQTest/obj/Debug/net6.0-windows/PowerLine.g.i.cs
@@ -53,7 +53,7 @@ namespace RabbitMQTest {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RabbitMQTest;component/powerline.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RabbitMQTest;V1.0.0.0;component/powerline.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\PowerLine.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
